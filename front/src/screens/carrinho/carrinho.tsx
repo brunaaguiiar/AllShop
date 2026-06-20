89cb0269d8941461177da9fae6f35927d4dca6f8
anlzgws <annaluiza.guimaraes2@gmail.com>
2026-06-20 16:14:31 -0300

    inicializando branch carrinho

---
+++ front/src/screens/carrinho/carrinho.tsx
@@ -0,0 +1,9 @@
+export default function Carrinho() {
+  return (
+    <main style={{ minHeight: "100vh", padding: "120px 32px 32px" }}>
+      <h1 style={{ fontSize: "2em", marginBottom: "6px" }}>
+        Carrinho de compras
+      </h1>
+    </main>
+  );
+}
