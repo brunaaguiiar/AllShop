17ab0c9303897f6310526a4bd77f47e954e9f1e8
unknown <ersousa203@gmail.com>
2026-06-21 15:36:55 -0300

    Implementa backend do carrinho de compras

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 
+import { carrinhoRoutes } from "./src/routes/Carrinho.routes";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(carrinhoRoutes);
 
 app.get("/", (req, res) => {
   res.send("API do AllShop funcionando 🚀");
---
+++ backend/src/routes/Carrinho.routes.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { CarrinhoController } from "../controller/CarrinhoController";
+
+const carrinhoRoutes = Router();
+
+const carrinhoController = new CarrinhoController();
+
+carrinhoRoutes.get(
+  "/carrinho",
+  carrinhoController.listarCarrinho.bind(carrinhoController)
+);
+
+carrinhoRoutes.post(
+  "/carrinho",
+  carrinhoController.adicionarItem.bind(carrinhoController)
+);
+
+carrinhoRoutes.delete(
+  "/carrinho/:id",
+  carrinhoController.removerItem.bind(carrinhoController)
+);
+
+export { carrinhoRoutes };
